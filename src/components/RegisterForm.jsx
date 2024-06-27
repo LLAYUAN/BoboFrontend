1378d1130781b6362bd45c84f489868640a4d009
DyingCoderLin <904573025@qq.com>
2024-06-27 15:21:54 +0800

    register

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,116 @@
+import React, {useMemo} from 'react';
+import {LockOutlined, UserOutlined,MailOutlined} from '@ant-design/icons';
+import {Button, Checkbox, Form, Input, ConfigProvider, notification} from 'antd';
+// import '../css/style.css'
+import {useNavigate} from "react-router-dom";
+
+const Context = React.createContext({
+    name: 'Default',
+});
+
+export default function RegisterForm({handleLogin}) {
+    const navigate = useNavigate();
+    const onFinish = (values) => {
+        handleLogin(values);
+    };
+
+    return (
+        <ConfigProvider
+            theme={{
+                components: {
+                    Form: {
+                        labelColor: 'rgba(0, 0, 0, 0.6)',
+                        labelFontSize: '18px',
+                    },
+                    Input: {
+                        inputFontSize: '16px',
+                    },
+                    Button: {
+                        defaultBorderColor: '#0958d9',
+                        defaultColor: '#0958d9',
+                        defaultHoverBg: '#0958d9',
+                        defaultHoverColor: '#ffffff',
+                        defaultActiveBg: '#0958d9',
+                        defaultActiveColor: '#ffffff',
+
+                    },
+                },
+            }}>
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Username!',
+                        },
+                    ]}
+                >
+                    <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username"/>
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Password!',
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="confirm"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please confirm your Password!',
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                        type="password"
+                        placeholder="Confirm Password"
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Email!',
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={<MailOutlined className="site-form-item-icon" />}
+                        type="email"
+                        placeholder="Email"
+                    />
+                </Form.Item>
+
+
+                <Form.Item>
+                    <Button type="default" htmlType="Login" className="login-form-button">
+                        注 册
+                    </Button>
+                    Or <a href="" onClick={() => navigate('/login')}>log in now!</a>
+                </Form.Item>
+            </Form>
+        </ConfigProvider>
+    );
+}
+
---
+++ src/view/register.jsx
@@ -0,0 +1,55 @@
+import LoginForm from "../components/LoginForm";
+import {Flex, notification} from 'antd';
+import bgImage from '../img/bg.jpg';
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+// import {register} from "../service/login";
+// import {setCookie} from "../service/cookie";
+import RigisterForm from "../components/RegisterForm";
+
+export default function RegisterPage({setIsLogin}) {
+    const navigate = useNavigate();
+    const handleRegister = (values) => {
+        // if (values.password !== values.confirm) {
+        //     notification.error({
+        //         message: '注册失败',
+        //         description: '两次输入密码不一致'
+        //     });
+        //     return;
+        // }
+        //
+        // register(values.username, values.password, values.email).then(data => {
+        //     if (data === "exist") {
+        //         notification.error({
+        //             message: '注册失败',
+        //             description: '用户名已存在'
+        //         });
+        //         return;
+        //     }
+        //     if (data === "success") {
+        //         notification.success({
+        //             message: '注册成功',
+        //             description: '请登录'
+        //         });
+        //         navigate("/login");
+        //         return;
+        //     }
+        // });
+    }
+
+    const backgroundStyle = {
+        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.1)), url(${bgImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '100vh', // 让背景充满整个视窗
+    };
+    return (
+        <Flex style={backgroundStyle} justify='center' align='center'>
+            <Flex justify='space-evenly' align='center' className="w-96 h-96 bg-white rounded-lg" vertical>
+                <p className="text-2xl">直播平台</p>
+                <RigisterForm handleLogin={handleRegister}/>
+            </Flex>
+        </Flex>
+    )
+}
