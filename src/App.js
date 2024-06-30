1e192729e8f89d554beeaa8f2aebd1acb430c481
h12321h <1656344825@qq.com>
2024-07-01 01:03:16 +0800

    update purple theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import AppRouter from './router/router';
+import {ConfigProvider} from "antd";
 
 function App() {
   return (
-     <AppRouter />
+      <ConfigProvider
+          theme={{
+            token: {
+                colorPrimary: '#531dab',
+                colorLink: '#531dab',
+
+             // borderRadius: 2,
+
+              // 派生变量，影响范围小
+              //colorBgContainer: '#fff',
+            },
+          }}
+
+      >
+       <AppRouter />
+      </ConfigProvider>
   );
 }
 
