f1a68d132b128b5b27d60989dfe4ee55b067f0b9
LLAYUAN <2235901996@qq.com>
2024-07-09 18:03:24 +0800

    7.9 18:00lay

--- src/service/uploadFile.js
+++ src/service/uploadFile.js
@@ -1,12 +1,13 @@
 import {RECORDVIDEO_PREFIX} from "./common";
 import axios from 'axios';
 export async function uploadFile(formData) {
+    console.log("调用uploadFile");
     const url = `${RECORDVIDEO_PREFIX}/uploadFile`;
     let token = `${localStorage.getItem('tokenHead')}${localStorage.getItem('token')}`;
     const response = await axios.post(url, formData, {
         headers: {
             'Content-Type': 'multipart/form-data', // 必须设置请求头
-            "Authorization": token
+            'Authorization': token
         },
         withCredentials: true,
     });
