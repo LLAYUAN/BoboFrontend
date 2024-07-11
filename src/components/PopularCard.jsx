582107e4b95c63333acac1b396b7a0dfc099f37e
LostOfMoon <1833775406@qq.com>
2024-07-11 09:56:57 +0800

    基本功能

--- src/components/PopularCard.jsx
+++ src/components/PopularCard.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
 import { Card, Avatar, Typography, Row, Col } from 'antd';
 import { YoutubeOutlined } from '@ant-design/icons';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const { Title, Text } = Typography;
 
-const PopularCard = ({roomID,title,author,rank,cover}) => {
-    console.log(cover);
-    const navigate=useNavigate();
+const PopularCard = ({ video }) => {
+    console.log(video);
+    const navigate = useNavigate();
     return (
         <Card
             hoverable
             style={{ width: 500, border: '1px solid #e8e8e8', borderRadius: '8px' }}
-            onClick={()=>{navigate(`/liveUser/${roomID}`)}}>
+            onClick={() => { navigate(`/liveUser/${video.id}`) }}>
             <Row>
                 <Col span={12} >
                     <div style={{ position: 'relative' }}>
                         <img
                             alt="example"
-                            src={cover}
+                            src={video.coverUrl}
                             style={{ width: '100%', height: '100%', borderRadius: '8px 0 0 8px' }}
                         />
                         <div style={{ position: 'absolute', bottom: 8, left: 0, background: 'rgba(0, 0, 0, 0.5)', borderRadius: '4px', padding: '2px 8px' }}>
-                            <Title level={5} style={{ margin: 0, color: '#fff' }}>{rank}</Title>
+                            <Title level={5} style={{ margin: 0, color: '#fff' }}>{video.hotIndex}</Title>
                         </div>
                     </div>
                 </Col>
-                <Col span={10} style={{paddingLeft:'20px'}}>
-                    <div style={{ display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
-                        <Title level={4} style={{ marginBottom: '8px' }}>{title}</Title>
-                        <Row align="middle" style={{paddingTop:'8px'}}>
+                <Col span={10} style={{ paddingLeft: '20px' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                        <Title level={4} style={{ marginBottom: '8px' }}>{video.roomName}</Title>
+                        <Row align="middle" style={{ paddingTop: '8px' }}>
                             <Col>
                                 <Avatar src="path_to_avatar_image" />
                             </Col>
-                            <Col flex="auto" style={{ paddingLeft: '8px'}}>
-                                <Text>{author}</Text>
+                            <Col flex="auto" style={{ paddingLeft: '8px' }}>
+                                <Text>{video.userName}</Text>
                             </Col>
                             {/*<Col>*/}
                             {/*    <YoutubeOutlined style={{ fontSize: '20px', color: '#ff4d4f' }} />*/}
--- src/view/category.jsx
+++ src/view/category.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { rank } from '../service/recommend';
 import VideoCard from "../components/VideoCard";
 import Autoplay from "../components/Autoplay";
-import {Row, Col, Divider} from 'antd';
+import { Row, Col, Divider } from 'antd';
 import CategorySelect from "../components/CategorySelect";
 
 
-const videoData = [
-    { roomID:1,title: 'React', author: 'Facebook' },
-    { roomID:1,title: 'Vue', author: 'Evan You' },
-    { roomID:1,title: 'Angular', author: 'Google' },
-    { roomID:1,title: 'Angular', author: 'Google' },
-    { roomID:1,title: 'Angular', author: 'Google' },
-    // 添加更多数据
-];
 export default function Category() {
+    const [selectedKey, setSelectedKey] = useState('0');
+    const [videoData, SetVideoData] = useState([]);
+
+    useEffect(() => {
+        const initVideoData = async () => {
+            SetVideoData(await rank(selectedKey));
+        }
+        initVideoData();
+    }, [selectedKey]);
+
     return (
         <div style={{ display: 'flex' }}>
             {/* 左侧固定的 CategorySelect */}
             <div style={{ position: 'fixed', width: '200px', height: '100vh', overflowY: 'auto' }}>
-                <CategorySelect />
+                <CategorySelect selectedKey={selectedKey} setSelectedKey={setSelectedKey} />
             </div>
 
             {/* 右侧可以滑动的部分 */}
@@ -26,7 +30,7 @@ export default function Category() {
                 <Row justify="space-between" gutter={[10, 30]}>
                     {videoData.map((video, index) => (
                         <Col key={index}>
-                            <VideoCard roomID={video.roomID} title={video.title} author={video.author} />
+                            <VideoCard video={video} />
                         </Col>
                     ))}
                 </Row>
