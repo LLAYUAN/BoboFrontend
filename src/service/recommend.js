b6ecbb1b69b5efec7bc21a8f4688561aba374236
LostOfMoon <1833775406@qq.com>
2024-07-11 16:54:09 +0800

    所有推荐算法

--- src/service/recommend.js
+++ src/service/recommend.js
@@ -4,3 +4,7 @@ export async function rank(tag) {
     let url = `${RECOMMENDPREFIX}/rank/${tag}`;
     return getJson(url);
 }
+export async function recommend() {
+    let url = `${RECOMMENDPREFIX}/rank/recommend`;
+    return getJson(url);
+}
