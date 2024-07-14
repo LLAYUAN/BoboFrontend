e83b43a67c0418f9c602a719db4c7e8ab44e9c82
DyingCoderLin <904573025@qq.com>
2024-07-14 17:07:35 +0800

    修改useBox逻辑

--- src/components/VideoLive.jsx
+++ src/components/VideoLive.jsx
@@ -3,8 +3,8 @@ import flvJs from 'flv.js';
 import {LIVEVIDEOPREFIX} from "../service/common"
 import {fetchCameraDevices, startCamera, startDesktop, record, stop} from "../service/livevideo"
 
-const RTMP = `rtmp://10.180.138.227:1935`;
-const HTTP = `http://10.180.138.227:8000`;
+const RTMP = `rtmp://192.168.0.241:1935`;
+const HTTP = `http://192.168.0.241:8000`;
 const PATH = `C:/Users`;
 
 const LiveDemo = ({ roomId }) => {
