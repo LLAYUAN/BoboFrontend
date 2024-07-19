35f4bc8751f154feb1660f189b699a8c8f20ee0d
zjh <zhangjh1901@sjtu.edu.cn>
2024-07-19 15:10:44 +0800

    zjh最终初稿

--- src/view/liveUser.jsx
+++ src/view/liveUser.jsx
@@ -25,7 +25,6 @@ export default function LiveUser() {
     const navigate = useNavigate();
     console.log(roomID);
 
-
     //自动获取房间信息
     useEffect(() => {
         fetchRoomInfo(roomID).then(response => {
